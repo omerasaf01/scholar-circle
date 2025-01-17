ec38caef81758c1729d5d0eb7d40de15ecb523f0
Ömer Asaf Karasu <omerasafkarasu06@gmail.com>
2025-01-18 02:47:27 +0300

    Theme wa changed and new post button was added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import UserCard from "@/components/user-card";
 import {PostPagination} from "@/components/pagination";
 import {getPosts} from "@/server-actions/post-actions";
 import {createClient} from "@/utils/supabase/server";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+import {Plus} from "lucide-react";
 
 export default async function Home() {
     let supabase = await createClient();
@@ -13,6 +16,13 @@ export default async function Home() {
     return (
         <div className="grid grid-cols-12 mt-12">
             <div className="col-span-6 col-start-2 space-y-3">
+                <div className="flex justify-end">
+                    <Link passHref legacyBehavior href={"/posts/add"}>
+                        <Button>
+                            <Plus /> Oluştur
+                        </Button>
+                    </Link>
+                </div>
                 {posts.length <= 0 && <span className="w-full flex justify-center">Postlar yüklenirken bir sorunla karşılaşıldı</span>}
                 {posts.map((value, index, array) => <PostCard  title={value.title} content={value.content} key={index}/>)}
                 <PostPagination />
